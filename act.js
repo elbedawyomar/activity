9fd5b895ed1d6f3ee13bbde74b5137abf92801a8
Pero <pierretameradel@gmail.com>
2025-02-07 20:23:12 +0200

    analyzing code

--- act.js
+++ act.js
@@ -1,1 +1,11 @@
-console.log('hello')
+
+function analyzeAsteroidData(diameters) {
+    if (!Array.isArray(diameters) || diameters.length === 0) {
+        return { totalCount: 0, averageDiameter: 0 };
+    }
+    const totalCount = diameters.length;
+    const totalDiameter = diameters.reduce((sum, diameter) => sum + diameter, 0);
+    const averageDiameter = totalDiameter / totalCount;
+    return { totalCount, averageDiameter };
+}
+module.exports = { analyzeAsteroidData };
